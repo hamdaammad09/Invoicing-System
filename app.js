68491c5d7859d84e74b029523c8f9e8c66c092bd
Hamda Ammad <hamdaammad09@gmail.com>
2025-07-28 15:44:06 +0500

    Initial commit

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://consultancy-frontend-eight.vercel.app'
+    'https://consultancy-frontend-eight.vercel.app',
+    'https://*.vercel.app' 
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
